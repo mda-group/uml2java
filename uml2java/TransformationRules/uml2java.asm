383565d326ac4d210226477df36b7d27f4d83b3d
tebmed <lamin.teb94@gmail.com>
2019-12-21 13:36:19 +0100

    generate Java class signature

--- uml2java/TransformationRules/uml2java.asm
+++ uml2java/TransformationRules/uml2java.asm
@@ -22,18 +22,61 @@
 		<constant value="self"/>
 		<constant value="GenerateJavaCode"/>
 		<constant value="MMM!Model;"/>
+		<constant value="Sequence"/>
+		<constant value="#native"/>
+		<constant value="0"/>
+		<constant value="ownedType"/>
+		<constant value="1"/>
+		<constant value="Class"/>
+		<constant value="J.oclIsTypeOf(J):J"/>
+		<constant value="B.not():B"/>
+		<constant value="16"/>
+		<constant value="CJ.including(J):CJ"/>
 		<constant value="/* &#10;"/>
 		<constant value=" * Automatically generated Java code with ATL &#10;"/>
 		<constant value="J.+(J):J"/>
-		<constant value=" * Authors: Mohammed TEBIB &amp; Pascal Andre &#10;"/>
 		<constant value=" */ &#10;"/>
-		<constant value="5:7-5:14"/>
-		<constant value="6:4-6:54"/>
-		<constant value="5:7-6:54"/>
-		<constant value="7:4-7:50"/>
-		<constant value="5:7-7:50"/>
+		<constant value=""/>
+		<constant value="2"/>
+		<constant value="3"/>
+		<constant value="visibility"/>
+		<constant value=" class "/>
+		<constant value="name"/>
+		<constant value="{&#10;  "/>
+		<constant value="&#10;} &#10;"/>
+		<constant value="5:32-5:36"/>
+		<constant value="5:32-5:46"/>
+		<constant value="5:59-5:60"/>
+		<constant value="5:73-5:81"/>
+		<constant value="5:59-5:82"/>
+		<constant value="5:32-5:83"/>
+		<constant value="6:7-6:14"/>
+		<constant value="7:4-7:54"/>
+		<constant value="6:7-7:54"/>
 		<constant value="8:4-8:12"/>
-		<constant value="5:7-8:12"/>
+		<constant value="6:7-8:12"/>
+		<constant value="9:47-9:49"/>
+		<constant value="9:5-9:12"/>
+		<constant value="9:50-9:60"/>
+		<constant value="10:12-10:14"/>
+		<constant value="10:12-10:25"/>
+		<constant value="9:50-10:25"/>
+		<constant value="10:27-10:36"/>
+		<constant value="9:50-10:36"/>
+		<constant value="10:38-10:40"/>
+		<constant value="10:38-10:45"/>
+		<constant value="9:50-10:45"/>
+		<constant value="11:9-11:16"/>
+		<constant value="9:50-11:16"/>
+		<constant value="12:9-12:17"/>
+		<constant value="9:50-12:17"/>
+		<constant value="9:5-13:12"/>
+		<constant value="6:7-13:12"/>
+		<constant value="5:6-13:12"/>
+		<constant value="c"/>
+		<constant value="it"/>
+		<constant value="Class_Code"/>
+		<constant value="classes"/>
 	</cp>
 	<operation name="1">
 		<context type="2"/>
@@ -70,23 +113,87 @@
 		<code>
 			<push arg="21"/>
 			<push arg="22"/>
-			<call arg="23"/>
-			<push arg="24"/>
-			<call arg="23"/>
-			<push arg="25"/>
-			<call arg="23"/>
+			<new/>
+			<load arg="23"/>
+			<get arg="24"/>
+			<iterate/>
+			<store arg="25"/>
+			<load arg="25"/>
+			<push arg="26"/>
+			<push arg="4"/>
+			<findme/>
+			<call arg="27"/>
+			<call arg="28"/>
+			<if arg="29"/>
+			<load arg="25"/>
+			<call arg="30"/>
+			<enditerate/>
+			<store arg="25"/>
+			<push arg="31"/>
+			<push arg="32"/>
+			<call arg="33"/>
+			<push arg="34"/>
+			<call arg="33"/>
+			<push arg="35"/>
+			<store arg="36"/>
+			<load arg="25"/>
+			<iterate/>
+			<store arg="37"/>
+			<load arg="36"/>
+			<load arg="37"/>
+			<get arg="38"/>
+			<call arg="33"/>
+			<push arg="39"/>
+			<call arg="33"/>
+			<load arg="37"/>
+			<get arg="40"/>
+			<call arg="33"/>
+			<push arg="41"/>
+			<call arg="33"/>
+			<push arg="42"/>
+			<call arg="33"/>
+			<store arg="36"/>
+			<enditerate/>
+			<load arg="36"/>
+			<call arg="33"/>
 		</code>
 		<linenumbertable>
-			<lne id="26" begin="0" end="0"/>
-			<lne id="27" begin="1" end="1"/>
-			<lne id="28" begin="0" end="2"/>
-			<lne id="29" begin="3" end="3"/>
-			<lne id="30" begin="0" end="4"/>
-			<lne id="31" begin="5" end="5"/>
-			<lne id="32" begin="0" end="6"/>
+			<lne id="43" begin="3" end="3"/>
+			<lne id="44" begin="3" end="4"/>
+			<lne id="45" begin="7" end="7"/>
+			<lne id="46" begin="8" end="10"/>
+			<lne id="47" begin="7" end="11"/>
+			<lne id="48" begin="0" end="16"/>
+			<lne id="49" begin="18" end="18"/>
+			<lne id="50" begin="19" end="19"/>
+			<lne id="51" begin="18" end="20"/>
+			<lne id="52" begin="21" end="21"/>
+			<lne id="53" begin="18" end="22"/>
+			<lne id="54" begin="23" end="23"/>
+			<lne id="55" begin="25" end="25"/>
+			<lne id="56" begin="28" end="28"/>
+			<lne id="57" begin="29" end="29"/>
+			<lne id="58" begin="29" end="30"/>
+			<lne id="59" begin="28" end="31"/>
+			<lne id="60" begin="32" end="32"/>
+			<lne id="61" begin="28" end="33"/>
+			<lne id="62" begin="34" end="34"/>
+			<lne id="63" begin="34" end="35"/>
+			<lne id="64" begin="28" end="36"/>
+			<lne id="65" begin="37" end="37"/>
+			<lne id="66" begin="28" end="38"/>
+			<lne id="67" begin="39" end="39"/>
+			<lne id="68" begin="28" end="40"/>
+			<lne id="69" begin="23" end="43"/>
+			<lne id="70" begin="18" end="44"/>
+			<lne id="71" begin="0" end="44"/>
 		</linenumbertable>
 		<localvariabletable>
-			<lve slot="0" name="18" begin="0" end="6"/>
+			<lve slot="1" name="72" begin="6" end="15"/>
+			<lve slot="3" name="73" begin="27" end="41"/>
+			<lve slot="2" name="74" begin="24" end="43"/>
+			<lve slot="1" name="75" begin="17" end="44"/>
+			<lve slot="0" name="18" begin="0" end="44"/>
 		</localvariabletable>
 	</operation>
 </asm>
